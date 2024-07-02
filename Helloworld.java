77d0e479779c625324958847c0ac90fdb0002337
rakeshs81 <131527560+rakeshs81@users.noreply.github.com>
2024-07-02 17:10:40 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,10 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
 	System.out.println("Hello Rakesh, welcome to Devops world!");
-	System.out.println("Hello Rakesh,  latest");
-	System.out.println("Hello Rakesh,  very latest");
 	System.out.println("satya added new code");
     }
 }
