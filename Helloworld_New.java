2718024be700e197601da7c8b7899041e0465a61
rakeshs81 <131527560+rakeshs81@users.noreply.github.com>
2024-08-16 16:51:53 +0530

    Update Helloworld_New.java

--- Helloworld_New.java
+++ Helloworld_New.java
@@ -5,5 +5,6 @@ class HelloWorld {
 	System.out.println("My First devops Project!");
 	System.out.println("satya added new code");
 	System.out.println("Kuna panda newly added now");
+	System.out.println("Deepti added now");
     }
 }
