18c733294ddb0ca9a1c53ba296a5813097b2a405
rakesh <rakesh81learning@gmail.com>
2024-08-16 16:21:30 +0530

    new file added

---
+++ Helloworld_New.java
@@ -0,0 +1,8 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("My First devops Project!");
+	#System.out.println("satya added new code");
+    }
+}
