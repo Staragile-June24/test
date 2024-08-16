0df71277d6febac14872c5c932e8a6fdd9b03b0e
rakeshs81 <131527560+rakeshs81@users.noreply.github.com>
2024-08-16 16:59:01 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("Hello Rakesh, welcome to Devops world!");
 	System.out.println("satya added new code");
+	System.out.println("Rakesh is an cloud architecture");    
     }
 }
