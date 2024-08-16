259c225f35bb633843a0ef94dee902c7815af387
rakesh <rakesh81learning@gmail.com>
2024-08-16 16:35:26 +0530

    comment removed now

--- Helloworld_New.java
+++ Helloworld_New.java
@@ -3,6 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("My First devops Project!");
-	#System.out.println("satya added new code");
+	System.out.println("satya added new code");
     }
 }
